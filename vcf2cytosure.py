eb6873630cd4bc7c53bbae9e8d29a23b59706b98
Marcel Martin <marcel.martin@scilifelab.se>
2017-04-10 15:43:23 +0200

    Fill in probes between events
    The probes are displayed at height 0, not at the correct coverage.

--- vcf2cytosure.py
+++ vcf2cytosure.py
@@ -17,7 +17,7 @@ Convert structural variants in a VCF to CGH (CytoSure) format
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 import sys
 import logging
-from collections import namedtuple
+from collections import namedtuple, defaultdict
 from io import StringIO
 from lxml import etree
 from cyvcf2 import VCF
@@ -26,6 +26,34 @@ PROBE_SPACING = 100000
 
 logger = logging.getLogger(__name__)
 
+
+# GRCh37
+CONTIG_LENGTHS = {
+	'1':  249_250_621,
+	'2':  243_199_373,
+	'3':  198_022_430,
+	'4':  191_154_276,
+	'5':  180_915_260,
+	'6':  171_115_067,
+	'7':  159_138_663,
+	'8':  146_364_022,
+	'9':  141_213_431,
+	'10': 135_534_747,
+	'11': 135_006_516,
+	'12': 133_851_895,
+	'13': 115_169_878,
+	'14': 107_349_540,
+	'15': 102_531_392,
+	'16':  90_354_753,
+	'17':  81_195_210,
+	'18':  78_077_248,
+	'19':  59_128_983,
+	'20':  63_025_520,
+	'21':  48_129_895,
+	'22':  51_304_566,
+	'X':  155_270_560,
+	'Y':   59_373_566,
+}
 
 Event = namedtuple('Event', ['chrom', 'start', 'end', 'type', 'info'])
 
@@ -264,14 +292,14 @@ def make_aberration(parent, chromosome, start, end, comment=None, method='VCF co
 	return aberration
 
 
-def spaced_probes(start, end):
+def spaced_probes(start, end, probe_spacing=PROBE_SPACING):
 	"""
 	Yield nicely spaced positions along the interval (start, end).
 	- start and end are always included
 	- at least three positions are included
 	"""
 	l = end - start
-	n = l // PROBE_SPACING
+	n = l // probe_spacing
 	spacing = l / max(n, 2)  # float division
 	i = 0
 	pos = start
@@ -300,6 +328,25 @@ def format_comment(info: dict) -> str:
 	return comment
 
 
+def merge_intervals(intervals):
+	"""Merge overlapping intervals into a single one"""
+	events = [(coord[0], 'START') for coord in intervals]
+	events.extend((coord[1], 'STOP') for coord in intervals)
+	events.sort()
+	active = 0
+	start = 0
+	for pos, what in events:
+		# Note adjacent 'touching' events are merged because 'START' < 'STOP'
+		if what == 'START':
+			if active == 0:
+				start = pos
+			active += 1
+		else:
+			active -= 1
+			if active == 0:
+				yield (start, pos)
+
+
 def main():
 	logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 	parser = HelpfulArgumentParser(description=__doc__)
@@ -314,6 +361,7 @@ def main():
 	probes = tree.xpath('/data/cgh/probes')[0]
 	submission = tree.xpath('/data/cgh/submission')[0]
 
+	chr_intervals = defaultdict(list)
 	for event in parse_vcf(args.vcf):
 		height = {'DEL': -1.0, 'DUP': +1.0, 'TDUP': +1.5, 'IDUP': +0.5, 'INV': -0.5, 'INS': -1.5}[event.type]
 		make_segment(segmentation, event.chrom, event.start, event.end, height)
@@ -325,10 +373,26 @@ def main():
 			for pos, height in triangle_probes(event.start):
 				make_probe(probes, event.chrom, pos, pos + 60, height, event.type)
 		else:
+			chr_intervals[event.chrom].append((event.start, event.end))
 			# show probes at slightly different height than segments
 			height *= 1.05
 			for pos in spaced_probes(event.start, event.end - 1):
 				make_probe(probes, event.chrom, pos, pos + 60, height, event.type)
+
+	def probes_between_events(chrom, start, end):
+		for pos in spaced_probes(start, end, probe_spacing=200000):
+			# CytoSure does not display probes at height=0.0
+			make_probe(probes, chrom, pos, pos + 60, 0.01, 'between events')
+
+	for chrom, intervals in chr_intervals.items():
+		if chrom not in CONTIG_LENGTHS:
+			continue
+		intervals = merge_intervals(intervals)
+		prev_end = 1
+		for start, end in intervals:
+			probes_between_events(chrom, prev_end, start - 1)
+			prev_end = end
+		probes_between_events(chrom, prev_end, CONTIG_LENGTHS[chrom])
 
 	tree.write(sys.stdout.buffer, pretty_print=True)
 
