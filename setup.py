5e5c3e3acb3220bd9527c01377aaa417bc29451d
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2018-11-27 18:51:52 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='vcf2cytosure',
-	version='0.3.0',
+	version='0.4.0',
 	author='Marcel Martin',
 	author_email='marcel.martin@scilifelab.se',
 	url='https://github.com/NBISweden/vcf2cytosure',
