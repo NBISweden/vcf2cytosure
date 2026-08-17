1a0ab46c1aec2d16daa9ea08308a0d3dd4a19617
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2026-08-17 11:07:08 +0200

    ruff format line-length 100

--- setup.py
+++ setup.py
@@ -3,26 +3,26 @@ from setuptools import find_packages, setup
 from vcf2cytosure.__version__ import __version__
 
 setup(
-	name='vcf2cytosure',
-	version=__version__,
-	author='Marcel Martin',
-	author_email='marcel.martin@scilifelab.se',
-	maintainer='Daniel Nilsson',
-	maintainer_email='daniel.nilsson@scilifelab.se',
-	url='https://github.com/NBISweden/vcf2cytosure',
-	description='Convert VCF with structural variations to CytoSure format',
-	#long_description=long_description,
-	license='MIT',
-	packages=find_packages(exclude=["tests/", "dist/", "build/"]),
-	install_requires=['lxml','pandas', 'cyvcf2'],
-	entry_points={'console_scripts': ['vcf2cytosure=vcf2cytosure.vcf2cytosure:main']},
-	classifiers=[
-		"Development Status :: 3 - Alpha",
-		"Environment :: Console",
-		"Intended Audience :: Science/Research",
-		"License :: OSI Approved :: MIT License",
-		"Natural Language :: English",
-		"Programming Language :: Python :: 3",
-		"Topic :: Scientific/Engineering :: Bio-Informatics"
-	]
+    name="vcf2cytosure",
+    version=__version__,
+    author="Marcel Martin",
+    author_email="marcel.martin@scilifelab.se",
+    maintainer="Daniel Nilsson",
+    maintainer_email="daniel.nilsson@scilifelab.se",
+    url="https://github.com/NBISweden/vcf2cytosure",
+    description="Convert VCF with structural variations to CytoSure format",
+    # long_description=long_description,
+    license="MIT",
+    packages=find_packages(exclude=["tests/", "dist/", "build/"]),
+    install_requires=["lxml", "pandas", "cyvcf2"],
+    entry_points={"console_scripts": ["vcf2cytosure=vcf2cytosure.vcf2cytosure:main"]},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
 )
